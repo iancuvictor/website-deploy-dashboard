5bef2ec438cf43fb05fa37edbbfe92b7562d7e00
Iancu Victor <iancuvictorwork@gmail.com>
2026-08-03 20:25:21 +0300

    multiple changes now, nothing big

--- frontend/src/components/deploymentsPage/deploymentPage/deploymentPage.tsx
+++ frontend/src/components/deploymentsPage/deploymentPage/deploymentPage.tsx
@@ -1,159 +1,40 @@
 import { useParams } from "react-router";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { type AxiosResponse, AxiosError } from "axios";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { GlobalStatesContext } from "../../../contexts/GlobalStatesContext";
 import ErrorPage from "../../errorPages/errorPage";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleNotch, faPlus, faX } from "@fortawesome/free-solid-svg-icons";
-import { toast } from "sonner";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import MainSettings from "./mainSettings";
 
 const API_URL = import.meta.env.VITE_API_URL;
-
-type Step = {
-    subPath: string,
-    command: string
-}
-
-type Form = {
-    targetType: string,
-    name: string,
-    deploymentUrl: string,
-    localPath: string,
-    remotePath: string,
-    host: string,
-    lastRunAt: string,
-    lastRunOutput: string,
-    lastRunStatus: string,
-    port: number,
-    privateKeyPath: string,
-    steps: Step[],
-    createdAt: string,
-    updatedAt: string,
-    username: string,
-    _id: string,
-}
 
 export default function DeploymentPage() {
     const { darkMode } = useContext(GlobalStatesContext);
     const { id } = useParams()
-
-    const queryClient = useQueryClient()
-
-    const formInputStyle = `px-3 py-2.5 ring-1 ring-neutral-700 rounded-xs text-[14px] w-full no-underline`
 
     const { data, isLoading, error } = useQuery<AxiosResponse, AxiosError<{ message: string }>>({
         queryKey: ['deployment', id],
         queryFn: () => axios.get(`${API_URL}/api/deployments/deployment?id=${id}`),
     });
 
-    let deploymentData = data?.data
-    const [form, setForm] = useState<Form>(deploymentData)
-
-    let path = form?.targetType === 'local' ? form?.localPath : form?.remotePath
-    let formPath = form?.targetType === 'local' ? 'localPath' : 'remotePath'
-
-    const updateDeploymentData = useMutation({
-        mutationFn: (form: Form) => axios.put(`${API_URL}/api/deployments/deployment?id=${id}`, form),
-        onSuccess: () => {
-            toast.success(`Data successfully updated`)
-        }
-    })
-
-    let currentStatus;
-
-    const deployWebsite = useMutation({
-        mutationFn: (id: string) => axios.post(`${API_URL}/api/deployments/deploy?id=${id}`),
-        onSuccess: (response) => {
-            queryClient.invalidateQueries({queryKey: ['deployment', id]})
-            toast.success(`Website successfully deployed`);
-            currentStatus = response.data.status;
-        }
-    })
-
-    useEffect(() => {
-        setForm(deploymentData)
-    }, [data])
-
-    let saveButtonActive = JSON.stringify(deploymentData) !== JSON.stringify(form)
-
     if (error) {
         return <ErrorPage error={error} />
     } else {
-        if (isLoading || form === undefined) {
+        if (isLoading || data === undefined) {
             return <div className="absolute flex flex-col items-center justify-center h-full w-full text-white text-[40px]">
                 <FontAwesomeIcon icon={faCircleNotch} spin />
             </div>
         } else {
-            return <div className={`${darkMode ? 'text-white' : 'text-black'} absolute flex flex-col gap-10 h-full w-full p-10`}>
+            return <div className={`${darkMode ? 'text-white' : 'text-black'} absolute flex flex-col items-center gap-10 h-full w-full p-10`}>
                 <div className="flex flex-col items-center justify-center">
-                    <h1 className="text-[30px] font-[700]">{form.name}</h1>
-                    <a href={deploymentData.deploymentUrl} target="_blank"
-                        className="cursor-pointer text-[16px] text-blue-400 hover:underline underline-offset-1">{form.deploymentUrl}</a>
+                    <h1 className="text-[30px] font-[700]">{data.data.name}</h1>
+                    <a href={data.data.deploymentUrl} target="_blank"
+                        className="cursor-pointer text-[16px] text-blue-400 hover:underline underline-offset-1">{data.data.deploymentUrl}</a>
                 </div>
-                <div className="flex flex-col gap-4 p-5 rounded-xs ring-1 ring-neutral-700 w-200">
-                    {/* <span 
-                    className={`${currentStatus === 'running' ? 'text-green-500' : 'text-white'}`}>
-                        Website is running</span> */}
-                    <span>Deployment type: {form.targetType}</span>
-                    <div className="flex flex-row gap-2 items-center">
-                        <span className="whitespace-nowrap">Root path:</span>
-                        <input type="text" value={path} onChange={(e) => setForm({ ...form, [formPath]: e.target.value })} className={formInputStyle} />
-                    </div>
-                    <div className="flex flex-row gap-2 items-center">
-                        <span className="whitespace-nowrap">Deployment URL:</span>
-                        <input type='text' value={form.deploymentUrl} onChange={(e) => setForm({ ...form, deploymentUrl: e.target.value })}
-                            className={`${formInputStyle} text-blue-400`} />
-                    </div>
-                    <button onClick={() => setForm({ ...form, steps: [...form.steps, { subPath: '', command: '' }] })
-                    }
-                        className="cursor-pointer hover:bg-neutral-900 ring-1 ring-neutral-700 p-2 w-fit">Add sub-route {" "}
-                        <FontAwesomeIcon icon={faPlus} /></button>
-                    <div className="flex flex-col gap-2">
-                        {form.steps.map((step, i) => {
-                            return <div key={i} className="flex flex-row items-center justify-center gap-3">
-                                <span>Route</span>
-                                <input type="text"
-                                    className={formInputStyle}
-                                    value={step.subPath || ''}
-                                    onChange={(e) => setForm({
-                                        ...form, steps: form.steps.map((step, index) => {
-                                            return index === i ? { ...step, subPath: e.target.value } : step
-                                        })
-                                    })}
-                                    placeholder="eg. /frontend" />
-                                <span>command</span>
-                                <input type="text"
-                                    className={formInputStyle}
-                                    value={step.command || ''}
-                                    onChange={(e) => setForm({
-                                        ...form, steps: form.steps.map((step, index) => {
-                                            return index === i ? { ...step, command: e.target.value } : step
-                                        })
-                                    })}
-                                    placeholder="eg. npm run dev" />
-                                <button onClick={() => setForm({ ...form, steps: form.steps.filter((step, index) => index !== i) })}
-                                    className="cursor-pointer bg-rose-500 hover:bg-rose-600 p-2 rounded-xs">
-                                    <FontAwesomeIcon icon={faX} />
-                                </button>
-                            </div>
-                        })}
-                    </div>
-                    <div className="w-full flex flex-row justify-between">
-                        <button disabled={!saveButtonActive} onClick={() => {
-                            console.log('hit');
-                            updateDeploymentData.mutate(form)
-                        }}
-                            className={`${saveButtonActive ? 'cursor-pointer hover:bg-green-500' : 'text-neutral-700'} 
-                        ring-1 ring-neutral-700 p-2 w-fit duration-75 ease-out`}>Save changes</button>
-
-
-                        <button onClick={() => deployWebsite.mutate(id)}
-                        className="cursor-pointer w-fit bg-green-500 hover:bg-green-600 shadow-lg/40 
-                        shadow-green-500 hover:shadow-green-600 ring-1 ring-neutral-700 p-2 rounded-xs">Deploy website</button>
-                    </div>
-                </div>
+                <MainSettings data={data} id={id} />
             </div>
         }
     }
